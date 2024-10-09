0f296f36b03cb2659c8b771ebff202de6f8acce2
ShabanaGT <shabana92banu@gmail.com>
2024-10-09 16:44:02 +0530

    Add files via upload

---
+++ age.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-age',
+  templateUrl: './age.component.html',
+  styleUrls: ['./age.component.scss']
+})
+export class AgeComponent implements OnInit {
+  age:any ;
+   showage:any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  agecalculator(){
+
+
+
+      let convertage = new Date(this.age);
+      
+
+      let timeDiff= Math.abs(Date.now()-convertage.getTime());
+
+      this.showage= Math.floor(timeDiff / (1000*3600*24)/365).toString() ;
+     
+
+  }
+
+}
+
+
+
+
+
